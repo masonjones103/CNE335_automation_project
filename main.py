b05a67c3f522bf803d50795e989de1783a8eb704
masonjones103 <117707387+masonjones103@users.noreply.github.com>
2025-03-17 13:38:56 -0700

    updated source

--- main.py
+++ main.py
@@ -12,3 +12,5 @@ if __name__ == '__main__':
     print_program_info()
     server = Server('35.88.147.193')
     print(server.ping())
+
+# code taken from https://stackoverflow.com/questions/2953462/pinging-servers-in-python
