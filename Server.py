6227099687ac160f87956f8ce15a9c4cb4de2421
masonjones103 <117707387+masonjones103@users.noreply.github.com>
2025-03-17 13:37:33 -0700

    v1
    Ping works

--- Server.py
+++ Server.py
@@ -1,10 +1,15 @@
+import platform
+import subprocess
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
     def __init__(self, server_ip):
-        # TODO -
         self.server_ip = server_ip
 
     def ping(self):
-        # TODO - Use os module to ping the server
-        return
+        param = '-n' if platform.system().lower() == 'windows' else '-c'
+        command = ['ping', param, '1', self.server_ip]
+
+        # returns True if ping worked
+        return subprocess.call(command) == 0
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-# This is the template code for the CNE335 Final Project
-# Justin Ellis
-# CNE 335 Fall
+# This is the code for the CNE335 Final Project
+# Mason Jones
+# CNE 335 Winter
+
+from Server import Server
 
 def print_program_info():
-    # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Mason Jones")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+    server = Server('35.88.147.193')
+    print(server.ping())
